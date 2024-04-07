0610ef2504e0f40e611d0186845dc6c4ffacd92f
kenig39 <96082144+kenig39@users.noreply.github.com>
2024-04-07 20:30:11 +0300

    add app

---
+++ Contact/Model.swift
@@ -0,0 +1,64 @@
+//
+//  model.swift
+//  Contact
+//
+//  Created by Alexander on 07.04.2024.
+//
+
+import Foundation
+
+protocol ContactProtocol {
+    var title: String {get set}
+    
+    var number: String {get set}
+}
+
+struct Contact: ContactProtocol {
+    var title: String
+    var number: String
+}
+
+protocol ContactStorageProtocol {
+    func load() -> [ContactProtocol]
+    
+    func save(contacts: [ContactProtocol])
+    
+}
+
+class ContactStorage: ContactStorageProtocol {
+    
+    private var storage = UserDefaults.standard
+    
+    var storageKey: String = "contacts"
+    
+    private enum ContactKey:String {
+        case title
+        case phone
+    }
+    
+    func load() -> [ContactProtocol] {
+        var resultContacts: [ContactProtocol] = []
+        let contactsFromStorage = storage.array(forKey: storageKey) as? [[String: String]] ?? []
+        for contact in contactsFromStorage {
+            guard let title = contact[ContactKey.title.rawValue],
+                  let phone = contact[ContactKey.phone.rawValue] else {
+                continue
+            }
+            resultContacts.append(Contact(title: title, number: phone))
+        }
+        return resultContacts
+    }
+    
+    func save(contacts: [ContactProtocol]) {
+        var arrayForStorage: [[String: String]] = []
+        contacts.forEach { contact in
+            var newElementForStorage: Dictionary<String, String> = [:]
+            newElementForStorage[ContactKey.title.rawValue] = contact.title
+            newElementForStorage[ContactKey.phone.rawValue] = contact.number
+            arrayForStorage.append(newElementForStorage)
+        }
+        storage.set(arrayForStorage, forKey: storageKey)
+    }
+}
+
+
---
+++ Contact/ViewController.swift
@@ -0,0 +1,122 @@
+//
+//  ViewController.swift
+//  Contact
+//
+//  Created by Alexander on 07.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+    var storage: ContactStorageProtocol!
+    
+    var tableView = UITableView()
+    
+    var contacts: [ContactProtocol] = [] {
+        didSet {
+            contacts.sort{ $0.title < $1.title}
+            // save contact
+            storage.save(contacts: contacts)
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+       
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        tableView.isHidden = true
+        tableView.backgroundColor = .green
+        
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "создать контакт", style: .done, target: self, action: #selector(showNewContactAlert))
+        
+        storage = ContactStorage() as ContactStorageProtocol
+        loadContacts()
+    }
+    
+  
+    
+    private func loadContacts() {
+        contacts = storage.load()
+    }
+    
+    
+    @objc func showNewContactAlert() {
+        let alertController = UIAlertController(title: "Создать новый контакт", message: "введите имя и тедлефон", preferredStyle: .alert)
+        
+        alertController.addTextField() { textfield in
+            textfield.placeholder = "имя"
+        }
+            alertController.addTextField() { textfield in
+                textfield.placeholder = "Номер телефона"
+                
+            }
+        
+        let createButton = UIAlertAction(title: "создать", style: .default, handler: { _ in
+            guard let contactName = alertController.textFields?[0].text,
+                  let contactPhone = alertController.textFields?[1].text else {
+                return
+            }
+            
+            let contact = Contact(title: contactName, number: contactPhone)
+            self.contacts.append(contact)
+            self.tableView.reloadData()
+            
+        })
+        
+        let cancelButton = UIAlertAction(title: "Отменить", style: .cancel, handler: nil)
+                
+                // добавляем кнопки в Alert Controller
+                alertController.addAction(cancelButton)
+                alertController.addAction(createButton)
+                
+                // отображаем Alert Controller
+                self.present(alertController, animated: true, completion: nil)
+    }
+}
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return 3
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           var cell: UITableViewCell
+           if let reuseCell = tableView.dequeueReusableCell(withIdentifier: "MyCell") {
+               print("Используем старую ячейку для строки с индексом \(indexPath.row)")
+               cell = reuseCell
+           } else {
+               print("Создаем новую ячейку для строки с индексом \(indexPath.row)")
+               cell = UITableViewCell(style: .default, reuseIdentifier: "MyCell")
+           }
+           configure(cell: &cell, for: indexPath)
+           return cell
+       }
+    
+    private func configure(cell: inout UITableViewCell, for indexPath: IndexPath) {
+           var configuration = cell.defaultContentConfiguration()
+           // имя контакта
+           configuration.text = contacts[indexPath.row].title
+           // номер телефона контакта
+        configuration.secondaryText = contacts[indexPath.row].number
+           cell.contentConfiguration = configuration
+       }
+}
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        // действие удаления
+        let actionDelete = UIContextualAction(style: .destructive, title: "Удалить") { _,_,_ in
+            // удаляем контакт
+            self.contacts.remove(at: indexPath.row)
+            // заново формируем табличное представление
+            tableView.reloadData()
+        }
+        // формируем экземпляр, описывающий доступные действия
+        let actions = UISwipeActionsConfiguration(actions: [actionDelete])
+        return actions
+    }
+}
